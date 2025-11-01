f89da47c062896bac0e3d0bd6d64fa7e0a54925c
Zaanian <sstauffer1@byupathway.edu>
2025-11-01 12:37:11 -0700

    save

--- scripts/home-dates.js
+++ scripts/home-dates.js
@@ -1,7 +1,4 @@
 
-
-
-
 const courses = [
     {
         subject: 'CSE',
@@ -81,3 +78,26 @@ const courses = [
         completed: false
     }
 ]
+
+// Set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//get the last modified date
+const lastmodifieddate = document.lastModified
+document.getElementById("lastmodified").textContent = ("Last modified " + lastmodifieddate)
+
+
+// hamburger button for menu in mobile size
+const hamButton = document.querySelector('#menu');
+const mainnav = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
+
+
+
+
+
+
